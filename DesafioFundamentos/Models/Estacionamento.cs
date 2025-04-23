a80c1a62494e6e45a500758e349f5324add35c87
Lucas Camargo <43620828+Lucasaucamargo@users.noreply.github.com>
2025-04-22 22:28:59 -0300

    Atualizar o Estacionamento.cs
    Inserir placa e colocar na lista de veículos

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos = console.readline();
+
         }
 
         public void RemoverVeiculo()
